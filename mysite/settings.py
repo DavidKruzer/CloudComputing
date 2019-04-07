08471800025cba4bceb03bacb1c5ab1b0d37802a
DavidKruzer <krusedav@gmx.de>
2019-04-07 21:33:33 +0200

    Delted Polls Applikation
    Improved Website Design
    
    Catches BadRequest Error, tells Browser
    
    Redirect for Main Page to translate App

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'translate.apps.TranslateConfig',
-    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,7 +44,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
--- translate/translater.py
+++ translate/translater.py
@@ -1,5 +1,6 @@
  # Imports the Google Cloud client library
 from google.cloud import translate as google_translate
+from google.api_core.exceptions import BadRequest
 from .models import Translation
 
 translate_client = google_translate.Client()
@@ -14,15 +15,20 @@ def translate(original, source_language, target_language):
     translation = cloud_translate(original, source_language, target_language);
     Translation.objects.create(original = original, translation = translation['text'], target_language = target_language, source_language = translation['language'])
     return translation
-def cloud_translate(original, source_language, target_language):  
-    if source_language == "":
-        translation = translate_client.translate(
-        original,
-        target_language=target_language)
-        source_language = translation['detectedSourceLanguage']
-    else:
-        translation = translate_client.translate(
-        original,
-        target_language=target_language,
-        source_language=source_language)
-    return {'text' : translation['translatedText'], 'language' : source_language};
+
+
+def cloud_translate(original, source_language, target_language):
+    try:
+        if source_language == "":
+            translation = translate_client.translate(
+            original,
+            target_language=target_language)
+            source_language = translation['detectedSourceLanguage']
+        else:
+            translation = translate_client.translate(
+            original,
+            target_language=target_language,
+            source_language=source_language)
+        return {'text' : translation['translatedText'], 'language' : source_language};
+    except BadRequest as e:
+        return {'text' : 'Error beim call der Google API:\n' + str(e), 'language' : source_language}; #if source language is undefined, the error applies for automatic source_language detection --> still works right
