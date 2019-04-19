1141799cbbb102fe09207ffe62451942f945acfa
DavidKruzer <krusedav@gmx.de>
2019-04-19 13:32:32 +0200

    Modified Settings to work in production mode depending on environment variable "DJANGO_DEBUG" and adding allowed host from environment variable "DJANGO_HOST"

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import requests
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-m=0$k!nq=o#&w9jc@9fx&maom7ch&v2uua)kg(77z1kj@yt0f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', os.environ.get('DJANGO_HOST', '')]
 
 
 # Application definition
